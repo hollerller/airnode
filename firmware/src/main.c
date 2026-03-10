f2f066b4b96144022c05ac865a512ffcf9a59c3b
David Tobasura <hollerller@gmail.com>
2026-03-10 10:30:36 -0500

    feat(fase-1): measure sleep current 2.37uA
    - Disabled UART console via Kconfig
    - k_sleep() 10s wake interval with led toggle
    - Sleep current 1.79uA avg, 4.17uA max
    - DoD achieved: <10uA

--- firmware/src/main.c
+++ firmware/src/main.c
@@ -1,13 +1,7 @@
 #include <zephyr/kernel.h>
-#include <zephyr/sys/printk.h>
-#include <zephyr/kernel.h>
-#include <zephyr/logging/log.h>
 #include <zephyr/drivers/gpio.h>
 
-LOG_MODULE_REGISTER(app, LOG_LEVEL_INF);
-
 #define LED0_NODE DT_ALIAS(led0)
-#define SLEEP_TIME_S 5
 #define WAKE_INTERVAL_MS 10000
 
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
@@ -16,8 +10,6 @@ int main(void)
 
 {
         int ret;
-
-        printk("Low power testing and current measurement");
 
         if (!gpio_is_ready_dt(&led))
         {
@@ -32,8 +24,6 @@ int main(void)
 
         while (1)
         {
-                uint32_t uptime = k_uptime_get_32();
-                LOG_INF("Wake up - uptime %u ms", uptime);
                 gpio_pin_toggle_dt(&led);
                 k_sleep(K_MSEC(WAKE_INTERVAL_MS));
         }
