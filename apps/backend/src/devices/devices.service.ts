d065134d0eef5d8bce2224cd455855d8ecf0c57b
David Tobasura <hollerller@gmail.com>
2026-03-21 13:02:38 -0500

    feat(fase-6): implement get and post for reading and devices

--- apps/backend/src/devices/devices.service.ts
+++ apps/backend/src/devices/devices.service.ts
@@ -1,15 +1,40 @@
 import { Injectable } from '@nestjs/common';
 import { CreateDeviceDto } from './dto/create-device.dto';
 import { UpdateDeviceDto } from './dto/update-device.dto';
+import { Device } from './entities/device.entity';
 
 @Injectable()
 export class DevicesService {
-  create(createDeviceDto: CreateDeviceDto) {
-    return 'This action adds a new device';
+  private tempDevices: Device[] = [
+    {
+      deviceId: 'airnode_00001',
+      deviceName: 'home',
+      firmwareVersion: '1.0.3',
+      batteryMv: 3100,
+      isOnline: true,
+      createdAt: new Date(2026, 3, 14, 10, 30, 0),
+      lastSeen: new Date(2025, 3, 21, 10, 24, 0),
+    },
+  ];
+
+  create(createDeviceDto: CreateDeviceDto): Device {
+    const newDevice: Device = {
+      deviceId: createDeviceDto.deviceId,
+      deviceName: createDeviceDto.deviceName,
+      firmwareVersion: createDeviceDto.firmwareVersion,
+      batteryMv: 0,
+      isOnline: false,
+      createdAt: new Date(),
+      lastSeen: new Date(),
+    };
+
+    this.tempDevices.push(newDevice);
+
+    return newDevice;
   }
 
-  findAll() {
-    return `This action returns all devices`;
+  findAll(): Device[] {
+    return this.tempDevices;
   }
 
   findOne(id: number) {
--- apps/backend/src/readings/readings.service.ts
+++ apps/backend/src/readings/readings.service.ts
@@ -1,15 +1,44 @@
 import { Injectable } from '@nestjs/common';
 import { CreateReadingDto } from './dto/create-reading.dto';
 import { UpdateReadingDto } from './dto/update-reading.dto';
+import { Reading } from './entities/reading.entity';
 
 @Injectable()
 export class ReadingsService {
-  create(createReadingDto: CreateReadingDto) {
-    return 'This action adds a new reading';
+  private tempReadings: Reading[] = [
+    {
+      deviceId: 'airnode_00001',
+      timestamp: new Date(),
+      createdAt: new Date(),
+      temperature_c: 25.75,
+      humidity_pct: 80.04,
+      pressure_hpa: 84.2,
+      pm1_0_ugm3: 10,
+      pm2_5_ugm3: 12,
+      pm10_ugm3: 12,
+    },
+  ];
+
+  create(createReadingDto: CreateReadingDto): Reading {
+    const newReading: Reading = {
+      deviceId: createReadingDto.deviceId,
+      timestamp: new Date(createReadingDto.timestamp),
+      createdAt: new Date(),
+      temperature_c: createReadingDto.temperature_c,
+      humidity_pct: createReadingDto.humidity_pct,
+      pressure_hpa: createReadingDto.pressure_hpa,
+      pm1_0_ugm3: createReadingDto.pm1_0_ugm3,
+      pm2_5_ugm3: createReadingDto.pm2_5_ugm3,
+      pm10_ugm3: createReadingDto.pm10_ugm3,
+    };
+
+    this.tempReadings.push(newReading);
+
+    return newReading;
   }
 
-  findAll() {
-    return `This action returns all readings`;
+  findAll(): Reading[] {
+    return this.tempReadings;
   }
 
   findOne(id: number) {
