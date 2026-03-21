23bbf2bfc5fee113dd48148bfc64d8bd41357bc1
David Tobasura <hollerller@gmail.com>
2026-03-21 11:54:26 -0500

    feat(fase-6): create Devices and Readings resources

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DevicesModule } from './devices/devices.module';
+import { ReadingsModule } from './readings/readings.module';
 
 @Module({
-  imports: [],
+  imports: [DevicesModule, ReadingsModule],
   controllers: [AppController],
   providers: [AppService],
 })
